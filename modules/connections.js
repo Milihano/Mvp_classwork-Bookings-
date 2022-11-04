9d1126e08a19f0b52112ff8bbd235111ac8f43e8
Your Name <you@example.com>
2022-11-04 16:55:15 +0100

    Finished The Api

--- modules/connections.js
+++ modules/connections.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
+  // password: process.env.DATABASE_PASSWORD,
   port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME
 })
